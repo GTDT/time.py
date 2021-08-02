a82685081eadbc46cddb0b2e80c3f4a70662d4be
GTDT <66834354+GTDT@users.noreply.github.com>
2021-08-03 00:58:41 +0300

    added some commetns :)

--- time.py
+++ time.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from time import sleep
-print("\033c")
-try:
-  while True:
-    sleep(0.1)
-    print('\x1b[H\n ',datetime.now(), end='')
+print("\033c")               # clear console
+try:                         # to display the exit message on exit
+  while True: 
+    print(                   # time printing
+      '\x1b[H\n',            # back to the start of the line and new line
+      datetime.now(),
+      end=''
+    );
+    sleep(0.1)               # delay between the prints
 except:
-  print('\n  exited...\n')
+  print('\n  exited...\n')   # exit message
